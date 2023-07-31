6a0f38494fd69c8ed2026ca82dbf3726dbe496d4
Megan Vincent <123343948+MVincent15@users.noreply.github.com>
2023-07-31 15:27:27 -0500

    added navbar and footer to app.js, implemented login/signup forms and added to mutations.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,44 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import Navbar from './components/navbar/navbar'
-import Footer from './components/footer/footer'
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Navbar from './components/navbar/navbar';
+import Footer from './components/footer/footer';
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-     < Navbar />
       <Router>
         <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Navbar />
           <Routes>
-          <Route 
-              
-            />
+            {/* Define your routes here */}
+            
+            
           </Routes>
         </div>
       </Router>
-      < Footer />
+      <Footer />
     </ApolloProvider>
   );
 }
