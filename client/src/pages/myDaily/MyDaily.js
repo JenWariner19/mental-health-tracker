bc9a3ea5d201236757a0dbdb0cb60879665d3f6a
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-08-04 12:15:53 -0500

    "Trying to get my changes pushed"

--- client/src/pages/myDaily/MyDaily.js
+++ client/src/pages/myDaily/MyDaily.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import CheckboxList from './Checklist';
 import MoodButton from './Mood';
 import WaterIntakeCheckbox from './Water';
-import MyThoughts from './MyJournalThoughts';
+import Gratefuls from './Gratefuls';
 import SleepCheckbox from './Sleep';
 
 import Auth from '../../utils/auth';
