85e0acd6e6c1f122dcccab41d56a7dba98a4ff62
Jen Wariner <jenniferwariner@yahoo.com>
2023-08-05 12:57:31 -0500

    setup structure for daily checklist

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 export default function Footer() {
   return (
     <footer>
-      <h2>Made by Annalee Beltran | Jen Wariner | Megan Vincent | Shannon Price </h2>
+      <h2>Made by: Annalee Beltran | Jen Wariner | Megan Vincent | Shannon Price </h2>
     </footer>
   );
 }
